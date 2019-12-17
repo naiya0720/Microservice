75608202a0ca27725f484f260074612c37fd6127
naiya0720 <1271753489@qq.com>
2019-12-17 18:56:32 +0800

    controller

--- springboot01/src/main/java/com/phn/springboot01/controller/IndexController.java
+++ springboot01/src/main/java/com/phn/springboot01/controller/IndexController.java
@@ -1,5 +1,7 @@
 package com.phn.springboot01.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -10,9 +12,11 @@ import java.util.Map;
  * @author Penghenan
  * @create 2019-12-14 21:00:53
  */
-@RestController
+@Slf4j
+@Controller
 public class IndexController {
 
+    @ResponseBody
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public Map<String,Object> home(){
         System.out.println("hello springBoot");
@@ -22,6 +26,7 @@ public class IndexController {
         return json;
     }
 
+    @ResponseBody
     @RequestMapping(value="/{id}", method=RequestMethod.GET)
     public Map<String,Object> getUser(@PathVariable Integer id) {
         // 处理"/users/{id}"的GET请求，用来获取url中id值的User信息
@@ -34,6 +39,12 @@ public class IndexController {
         return json;
     }
 
+
+    @RequestMapping("/index.do")
+    public String toIndex(){
+        log.info(">>>>>  hello index");
+        return "index";
+    }
 
 
 
